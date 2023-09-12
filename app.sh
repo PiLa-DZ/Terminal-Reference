9e1a13e5eb7510447629e579ca05bb7f0ebaa208
PiLa-DZ <black.henter.hat@gmail.com>
2023-09-12 15:58:52 +0100

    m

--- app.sh
+++ app.sh
@@ -1,18 +1,32 @@
-# Variabls
+#!/bin/bash
+
+
+
+
+echo '-- Variabls ------------------------'
+
 my_var="PiLa"
 echo "Hi $my_var"
 where_am_i=$(pwd)
 echo "You are in $where_am_i"
 
 
-# Math Function
+
+
+
+echo '-- Math Function -------------------'
+
 expr 3 +  3
 expr 3 -  3
 expr 3 \* 3
 expr 3 /  3
 expr 3 %  3
 
-echo '------------------------------------'
+
+
+
+echo '-- If Condition ---------------------'
+
 my_file=/usr/bin/vim
 if [ -f $my_file ] ; then
         echo "It's work 1"
@@ -24,4 +38,16 @@ fi
 if command -v vim ; then
         echo "It's work 3"
 fi
+
+
+
+
+echo '-- For Loops -------------------------'
+
+for i in {1..10}; do
+	echo $i
+done
+
+
+
 
