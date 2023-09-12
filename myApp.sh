cbc549cf28bedc708fcb0ed3568d76cd54aea1f3
PiLa-DZ <black.henter.hat@gmail.com>
2023-09-12 11:52:14 +0100

    m

--- myApp.sh
+++ myApp.sh
@@ -13,10 +13,15 @@ expr 3 /  3
 expr 3 %  3
 
 echo '------------------------------------'
-if [ /usr/bin/vim ] ; then
+my_file=/usr/bin/vim
+if [ -f $my_file ] ; then
         echo "It's work 1"
 fi
+my_dir=nothig
+if [ -d $my_dir ] ; then
+	echo "It's work 2"
+fi
 if command -v vim ; then
-        echo "It's work 2"
+        echo "It's work 3"
 fi
 
